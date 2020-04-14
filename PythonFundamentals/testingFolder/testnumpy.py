d2032b8309f4cf795fbf2dd4855b9d73d79b4f0c
Marty <Marty@DESKTOP-N1M9EJ9>
2020-04-14 08:45:04 +0100

    test numpy

---
+++ PythonFundamentals/testingFolder/testnumpy.py
@@ -0,0 +1,16 @@
+'''
+Created on 14 Apr 2020
+
+@author: Marty
+'''
+import numpy as np # numpy is external library and need to be imported
+
+
+# simple list 
+list = [1,2,3]
+
+# variable of numpy array type take list as argument
+x = np.array(list)
+
+print(type(x))
+print(x)
